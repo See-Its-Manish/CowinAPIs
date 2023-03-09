9cb07b2305fed3254f7f27c10d4f962b1bb6b026
See-Its-Manish <seeitsmanish@gmail.com>
2023-03-09 23:37:42 +0530

    Scheduled cronjob for every hour

--- src/utils/job.js
+++ src/utils/job.js
@@ -5,7 +5,7 @@ const reminderService = new ReminderService();
 const {Center} = require('../models/index');
 const setupJobs = async () => {
 
-    cron.schedule('*/1 * * * *', async() => {
+    cron.schedule('0 * * * *', async() => {
 
         const reminders = await reminderService.getAllReminders();
         
